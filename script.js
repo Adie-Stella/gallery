fa2fbadce8d2c0528e724f3c92465104e161bfbe
Adie-Stella <stellattoe@gmail.com>
2025-04-19 16:30:19 +0300

    Food Gallery

---
+++ script.js
@@ -0,0 +1,83 @@
+const change =document.getElementById("food1");
+
+change.addEventListener("click", (event) => {
+    event.target.style.background = "linear-gradient(to right, #fff,rgb(99, 228, 215),rgb(79, 0, 143))";
+    event.target.style.color = "rgb(79, 0, 143)";
+});
+
+const change2 =document.getElementById("food2");
+
+change2.addEventListener("click", (event) => {
+    event.target.style.background = "linear-gradient(to right, #fff,rgb(99, 228, 215),rgb(79, 0, 143))";
+    event.target.style.color = "rgb(79, 0, 143)";
+});
+
+const change3 = document.getElementById('food3');
+
+change3.addEventListener("click", (event) => {
+    event.target.style.background = "linear-gradient(to right, #fff,rgb(99, 228, 215),rgb(79, 0, 143))";
+    event.target.style.color = "rgb(79, 0, 143)";
+});
+
+const change4 = document.getElementById('food4');
+
+change4.addEventListener('click', (event) =>{
+    event.target.style.background = "linear-gradient(to right, #fff,rgb(99, 228, 215),rgb(79, 0, 143))";
+    event.target.style.color = "rgb(79, 0, 143)";
+});
+
+const change5 = document.getElementById("food5");
+
+change5.addEventListener("click", (event) => {
+    event.target.style.background = "linear-gradient(to right, #fff,rgb(99, 228, 215),rgb(79, 0, 143))";
+    event.target.style.color = "rgb(79, 0, 143)";
+});
+
+const change6 = document.getElementById("food6");
+
+change6.addEventListener("click", (event) => {
+    event.target.style.background = "linear-gradient(to right, #fff,rgb(99, 228, 215),rgb(79, 0, 143))";
+    event.target.style.color = "rgb(79, 0, 143)";
+});
+
+const change7 = document.getElementById("food7");
+
+change7.addEventListener("click", (event) => {
+    event.target.style.background = "linear-gradient(to right, #fff,rgb(99, 228, 215),rgb(79, 0, 143))";
+    event.target.style.color = "rgb(79, 0, 143)";
+});
+
+const change8 = document.getElementById("food8");
+
+change8.addEventListener("click", (event) => {
+    event.target.style.background = "linear-gradient(to right, #fff,rgb(99, 228, 215),rgb(79, 0, 143))";
+    event.target.style.color = "rgb(79, 0, 143)";
+});
+
+const change9 = document.getElementById("food9");
+
+change9.addEventListener("click", (event) => {
+    event.target.style.background = "linear-gradient(to right, #fff,rgb(99, 228, 215),rgb(79, 0, 143))";
+    event.target.style.color = "rgb(79, 0, 143)";
+});
+
+const change10 = document.getElementById("food10");
+
+change10.addEventListener("click", (event) => {
+    event.target.style.background = "linear-gradient(to right, #fff,rgb(99, 228, 215),rgb(79, 0, 143))";
+    event.target.style.color = "rgb(79, 0, 143)";
+});
+
+const change11 = document.getElementById("food11");
+
+change11.addEventListener("click", (event) => {
+    event.target.style.background = "linear-gradient(to right, #fff,rgb(99, 228, 215),rgb(79, 0, 143))";
+    event.target.style.color = "rgb(79, 0, 143)";
+});
+
+const change12 = document.getElementById("food12");
+
+change12.addEventListener("click", (event) =>{
+    event.target.style.background = "linear-gradient(to right, #fff,rgb(99, 228, 215),rgb(79, 0, 143))";
+    event.target.style.color = "rgb(79, 0, 143)";
+});
